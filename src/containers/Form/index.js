1de5c6874acb10aaeb8187621a7c3f9e1c69174d
Reymg22 <117723925+Reymg22@users.noreply.github.com>
2023-10-13 13:06:55 +0200

    Form Updated

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -2,19 +2,12 @@ import { useCallback, useState } from "react";
 import PropTypes from "prop-types";
 import Field, { FIELD_TYPES } from "../../components/Field";
 import Button, { BUTTON_TYPES } from "../../components/Button";
+import Select from "../../components/Select";
 
 const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
-
-  // State for managing the checkbox value
-  const [isCheckboxChecked, setCheckboxChecked] = useState(false);
-
-  const handleCheckboxChange = useCallback((event) => {
-    setCheckboxChecked(event.target.checked);
-  }, []);
-
   const sendContact = useCallback(
     async (evt) => {
       evt.preventDefault();
@@ -39,11 +32,12 @@ const Form = ({ onSuccess, onError }) => {
           <Field placeholder="" label="Nom" />
           <Field placeholder="" label="Prénom" />
 
-          {/* Add onChange handler for the checkbox */}
-          <input
-            type="checkbox"
-            checked={isCheckboxChecked}
-            onChange={handleCheckboxChange}
+          <Select
+            selection={["Personel", "Entreprise"]}
+            onChange={() => null}
+            label="Personel / Entreprise"
+            type="large"
+            titleEmpty
           />
 
           <Field placeholder="" label="Email" />
